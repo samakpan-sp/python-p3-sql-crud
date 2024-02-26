1c7412829818f2d572125ff2a0c1a92aee84249f
mrSam <samakpan817@gmail.com>
2024-02-26 12:55:41 +0100

    Completed CRUD code along exercise

---
+++ add_cat_database.sql
@@ -0,0 +1,3 @@
+INSERT INTO cats(name, age, breed) VALUES ('Lil Bub', 5, 'merican Shorthair');
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+INSERT INTO cats (name, age, breed) VALUES ('Annah', 8, 'Tab');
